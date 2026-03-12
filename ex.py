89593bd9277ab75a19573d2d4c2322f03b7616f5
BlackAlexandre <159321029+BlackAlexandre@users.noreply.github.com>
2026-03-12 19:43:35 -0300

    aa

--- ex.py
+++ ex.py
@@ -1,1 +1,5 @@
-print ("hello word")
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+
+soma = v1+ v2
+print (soma)
---
+++ ex2.py
@@ -0,0 +1,5 @@
+v1 = float("digite o valor 1:")
+v2 = float("digite o valor 2:")
+
+multiplicação = (v1 * v2)
+print (multiplicação)
